f1c7ed52f57143c110f27f87874e740c23809aeb
EdsonMoreiraAds <amzmasterads@gmail.com>
2024-08-19 12:11:38 -0300

    Finalização do to do list

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
-function novaTarefa(){
+function novaTarefa() {
     overlay.classList.add("active");
     modal.classList.add("active");
     criar.classList.add("active");
 }
 
-function confirmarExclusao(){
+function confirmarExclusao(id) {
     overlay.classList.add("active");
     modal.classList.add("active");
     confirmacao.classList.add("active");
+    deletedID = id;
 }
 
-function fecharModal(){
+function fecharModal() {
     overlay.classList.remove("active");
     modal.classList.remove("active");
     criar.classList.remove("active");
@@ -18,30 +19,61 @@ function fecharModal(){
 }
 
 document.addEventListener('keyup', (evento) => {
-    if(evento.key == "Escape"){
+    if (evento.key == "Escape") {
         fecharModal();
-    }   
+    }
 });
 
-let tarefas = [
-    {
-        titulo: "Pendurar quadro",
-        descricao: "Furar dois furos na parede para colocar as buchas e pendurar o quadro"
-    },
-    {
-        titulo: "Comer",
-        descricao: "Em casa"
-    },
-    {
-        titulo: "Reunião com cliente",
-        descricao: "Ir na KF veiculos se reunir com fabricio"
-    },
-    {
-        titulo: "Estudar",
-        descricao: "Entender melhor variaveis e constantes"
-    },
-    {
-        titulo: "Comprar capacete",
-        descricao: "Ir na loja e comprar o capacete que EU já escolhi e não ceder ao consumismo"
-    },
-];
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+let deletedID = 0;
+
+function listarTarefas() {
+        lista.innerHTML = "";
+
+    tarefas.map((tarefa) => {
+        lista.innerHTML += `
+            <li>
+                <div>
+                    <h5>${tarefa.titulo}</h5>
+                    <p>${tarefa.descricao}</p>
+                </div>
+                <div>
+                    <box-icon name='trash' onclick="confirmarExclusao(${tarefa.id})"></box-icon>
+                </div>
+            </li>        
+        `;
+    })
+}
+listarTarefas();
+
+function adicionarTarefa(){
+    event.preventDefault();
+    let tarefa = { 
+        id: tarefas.length + 1,
+        titulo: titulo.value,
+        descricao: descricao.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    fecharModal();
+    listarTarefas();
+}
+function deletarTarefa(){
+    let tarefasFiltradas = [];
+    tarefasFiltradas = tarefas.filter(tarefa =>{
+        if(tarefa.id != deletedID){
+            return tarefa;
+        }
+   
+    })
+    tarefas = tarefasFiltradas;
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    fecharModal();
+    listarTarefas();
+}
+
+
+   
+
+
